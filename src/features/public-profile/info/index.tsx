9efa9d6cb982c9b2d7015de59251f0a76c807964
Naim lasker <naimlasker@gmail.com>
2023-04-25 00:01:03 +0600

    profile page updated

--- src/features/public-profile/info/index.tsx
+++ src/features/public-profile/info/index.tsx
@@ -6,7 +6,7 @@ const Info = ({ userDetails }: any) => {
     <>
       <div className="flex flex-wrap justify-center">
         <div className="w-full lg:w-4/12 px-4 lg:order-1">
-          <div className="flex justify-center py-4 lg:pt-4 pt-8">
+          {/* <div className="flex justify-center py-4 lg:pt-4 pt-8">
             <div className="mr-4 p-3 text-center">
               <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span>
               <span className="text-sm text-blueGray-400">Videos</span>
@@ -19,7 +19,7 @@ const Info = ({ userDetails }: any) => {
               <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span>
               <span className="text-sm text-blueGray-400">Comments</span>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
@@ -35,9 +35,9 @@ const Info = ({ userDetails }: any) => {
         </div>
 
         <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-          <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+          {/* <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
             Subscribe
-          </button>
+          </button> */}
         </div>
       </div>
     </>
