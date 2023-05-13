e4586d194f1e23f41af56400a6fa5a2472f7e51a
Naim lasker <naimlasker@gmail.com>
2023-05-14 02:01:48 +0600

    get profile data into edit input field and update avatar

--- src/features/public-profile/about/index.tsx
+++ src/features/public-profile/about/index.tsx
@@ -3,14 +3,77 @@ import { PrimaryTextArea } from '@/components/core/textarea/textarea';
 import { selectAuthUser } from '@/slices/authSlice';
 import { Dialog, Transition } from '@headlessui/react';
 import { ArrowUpTrayIcon, PencilIcon } from '@heroicons/react/20/solid';
-import React, { Fragment, useState } from 'react'
+import React, { ChangeEvent, Fragment, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 
 const About = ({ username, userDetails }: any) => {
   const authUser = useSelector(selectAuthUser);
+  let [jwt, setJwt] = useState('')
+  let [updatedPhoto, setUpdatedPhoto] = useState('')
   let [showEditModal, setShowEditModal] = useState(false)
 
+  const fetchUserMetaData = async () => {
+    const { getUserGlobalMetadata, identity } = await import('deso-protocol')
+    const JWT = await identity.jwt()
+    setJwt(JWT)
+
+    const params = {
+      JWT,
+      UserPublicKeyBase58Check: userDetails?.Profile?.PublicKeyBase58Check,
+    }
+
+    const response = await getUserGlobalMetadata(params)
+    console.log('metadata response', response);
+
+  }
+
+  useEffect(() => {
+    fetchUserMetaData()
+  }, [])
+
+  const uploadPhoto = async () => {
+    const { updateProfile,  } = await import('deso-protocol')
+    const params = {
+      ExtraData:
+      {
+        LargeProfilePicURL: "https://images.deso.org/c11f48ada993e2b0c37b5befe2da744d221d1ea3d77234892ff97e20a94e215a.webp"
+      },
+      IsHidden : false,
+      MinFeeRateNanosPerKB : 1000,
+      NewCreatorBasisPoints : 10000,
+      NewDescription : "",
+      NewProfilePic : "https://images.deso.org/c11f48ada993e2b0c37b5befe2da744d221d1ea3d77234892ff97e20a94e215a.webp",
+      NewStakeMultipleBasisPoints : 12500,
+      NewUsername : "",
+      ProfilePublicKeyBase58Check
+        :
+        "",
+      UpdaterPublicKeyBase58Check
+        :
+        "BC1YLfsXHb15rC9FCtmy4QbjZ5YSibqQnqjAJALkbVeWu221wGUgnP9"
+    }
+
+    const params2 = {
+      TransactionHex: ''
+    }
+  }
+
   console.log('authUser', authUser);
+
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    // Handle the selected file here
+    const file: any = event.target.files?.[0];
+    console.log(file);
+    const { uploadImage } = await import('deso-protocol')
+    const params = {
+      UserPublicKeyBase58Check: userDetails?.Profile?.PublicKeyBase58Check,
+      JWT: jwt,
+      file
+    }
+    const response = await uploadImage(params)
+    setUpdatedPhoto(response?.ImageURL)
+  };
 
 
   return (
@@ -64,13 +127,25 @@ const About = ({ username, userDetails }: any) => {
 
                   <div className="mt-2 flex flex-col justify-center items-start">
                     <img alt="..."
-                      src={userDetails?.Avatar}
+                      src={updatedPhoto ? updatedPhoto : userDetails?.Avatar}
                       className=" shadow-xl rounded-full h-[120px] w-[120px] align-middle border-none mx-auto" />
 
-                    <button className="flex items-center justify-center mx-auto mt-3 bg-blue-300 px-5 py-2 text-black rounded-md">
-                      <ArrowUpTrayIcon className="w-4 h-4" />
-                      <span className="ml-2">Upload Photo</span>
-                    </button>
+                    <div className="text-center flex justify-center w-full">
+                      <input
+                        type="file"
+                        id="fileInput"
+                        className="hidden"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                      />
+                      <label
+                        htmlFor="fileInput"
+                        className="flex items-center justify-center mx-auto mt-3 bg-blue-300 px-5 py-2 text-black rounded-md cursor-pointer"
+                        onClick={uploadPhoto}>
+                        <ArrowUpTrayIcon className="w-4 h-4" />
+                        <span className="ml-2">Upload Photo</span>
+                      </label>
+                    </div>
                   </div>
 
 
@@ -78,22 +153,26 @@ const About = ({ username, userDetails }: any) => {
                     <PrimaryInput
                       label="Username"
                       className="w-full mb-4"
-                      placeholder="Username"/>
+                      placeholder="Write username"
+                      value={userDetails?.Profile?.Username} />
 
                     <PrimaryTextArea
                       label="Description"
                       className="w-full mb-4 min-h-[100px]"
-                      placeholder="Description"/>
+                      placeholder="Write description"
+                      value={userDetails?.Profile?.Description} />
 
                     <PrimaryInput
                       label="Founder reward percentage"
                       className="w-full mb-4"
-                      placeholder="Description"/>
+                      placeholder="Write reward percentage"
+                      value={(userDetails?.Profile?.CoinEntry?.CreatorBasisPoints) / 100} />
 
                     <PrimaryInput
                       type="email"
                       label="Email"
-                      className="w-full" />
+                      className="w-full"
+                      placeholder="Write your email" />
                   </div>
 
                   <div className="mt-10 text-center">
