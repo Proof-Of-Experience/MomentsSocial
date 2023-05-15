52d296380dfb033cb522f87f3682f0ded64c55ed
Naim lasker <naimlasker@gmail.com>
2023-05-16 00:03:05 +0600

    reaction design added

--- src/components/snippets/video/index.tsx
+++ src/components/snippets/video/index.tsx
@@ -1,40 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { VideoItemProps } from '@/model/video'
 
+interface ReactionButtonProps {
+	onSelect?: (reaction: string) => void;
+}
+
+const ReactionButton: React.FC<ReactionButtonProps> = ({ onSelect }) => {
+
+	const handleReactionSelect = (selectedReaction: string) => {
+		if (onSelect) {
+			onSelect(selectedReaction);
+		}
+	};
+
+	const emojiList = ['👍', '❤️', '😂', '😮‍💨', '😢', '😡', '😀'];
+
+	return (
+		<div
+			className="emoji-reaction"
+		>
+			<div className="emoji-list">
+				{emojiList.map((emoji) => (
+					<button key={emoji} onClick={() => handleReactionSelect(emoji)}>
+						{emoji}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+}
+
+
 const VideoItem = ({ item }: VideoItemProps) => {
+	const [currentReaction, setcurrentReaction] = useState('👍')
+	const [isReactionHovered, setIsReactionHovered] = useState(false)
 
-  return (
-    <div>
-      <div className="relative overflow-hidden w-full pt-[75%]">
-        <iframe
-          className="absolute top-0 left-0 right-0 bottom-0 w-full h-full"
-          src={`${item?.VideoURLs[0]}`}
-          title="YouTube video player"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen>
-        </iframe>
-      </div>
+	const handleButtonHover = () => {
+		setIsReactionHovered(true)
+	}
 
-      <h4 className="mt-2 text-md font-bold capitalize">{item?.ProfileEntryResponse?.Username}</h4>
-      <p className="text-sm line-clamp-2 text-left min-h-[40px]">{item?.Body}</p>
+	const handleButtonMouseLeave = () => {
+		setIsReactionHovered(false)
+	}
 
-      <div className="flex justify-between mt-2">
-        <button className="flex items-center text-lg font-bold text-gray-700">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
-          </svg>
-          <span className="ml-1">{item?.LikeCount}</span>
-        </button>
+	const handleReactionSelect = (selectedReaction: string) => {
+		setcurrentReaction(selectedReaction)
+	}
 
-        <button className="flex items-center text-lg font-bold text-gray-700">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-          </svg>
-          <span className="ml-1">{item?.CommentCount}</span>
-        </button>
-      </div>
-    </div>
-  )
+	return (
+		<div className="relative">
+
+			<div className="relative overflow-hidden w-full pt-[75%]">
+				<iframe
+					className="absolute top-0 left-0 right-0 bottom-0 w-full h-full"
+					src={`${item?.VideoURLs[0]}`}
+					title="YouTube video player"
+					allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen>
+				</iframe>
+			</div>
+
+			<h4 className="mt-2 text-md font-bold capitalize">{item?.ProfileEntryResponse?.Username}</h4>
+			<p className="text-sm line-clamp-2 text-left min-h-[40px]">{item?.Body}</p>
+
+			<div className="flex justify-between mt-2">
+				<button
+					className="flex items-center text-lg font-bold text-gray-700"
+					onMouseEnter={handleButtonHover}
+					onMouseLeave={handleButtonMouseLeave}>
+					{currentReaction}
+					<span className="ml-1">{item?.LikeCount}</span>
+
+					{isReactionHovered && (
+						<div className={`absolute bottom-7 left-0 bg-white rounded-3xl border-2 shadow px-3`}>
+							<ReactionButton onSelect={handleReactionSelect} />
+						</div>
+					)}
+				</button>
+
+				<button className="flex items-center text-lg font-bold text-gray-700">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+					</svg>
+					<span className="ml-1">{item?.CommentCount}</span>
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default VideoItem
