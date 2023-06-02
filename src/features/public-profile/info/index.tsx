157ee40010acb48d97138f5506878c125796c762
Naim lasker <naimlasker@gmail.com>
2023-06-03 00:49:40 +0600

    view activity added & buy removed

--- src/features/public-profile/info/index.tsx
+++ src/features/public-profile/info/index.tsx
@@ -12,7 +12,6 @@ import { nanosToUSD } from '@/utils'
 const Info = ({ userDetails, username }: any) => {
   const authUser = useSelector(selectAuthUser)
   const [showEditModal, setShowEditModal] = useState<boolean>(false)
-  const [showBuyModal, setShowBuyModal] = useState<boolean>(false)
   const [editProfileData, setEditProfileData] = useState<EditProfileProps>({
     userName: userDetails?.Profile?.Username,
     description: userDetails?.Profile?.Description,
@@ -146,17 +145,18 @@ const Info = ({ userDetails, username }: any) => {
           authUser?.currentUser?.ProfileEntryResponse?.Username === username ?
             <div className="flex">
               <button
-                className="flex items-center border-2 px-8 py-3 rounded-md mr-3"
+                className="flex items-center border-2 px-4 py-1 text-sm rounded-md mr-3"
                 onClick={() => setShowEditModal(true)}>
                 <PencilIcon className="w-4 h-4" />
                 <span className="ml-3">Edit</span>
               </button>
 
-              <button
-                className="f bg-blue-500 text-white px-8 py-3 rounded-md"
-                onClick={() => setShowBuyModal(true)}>
-                <span className="ml-3">Buy</span>
-              </button>
+              <a
+                target="_blank"
+                href={`https://wallet.deso.com/?user=${username}&tab=activity`}
+                className="f bg-blue-500 text-white px-4 py-1 text-sm rounded-md">
+                <span className="ml-3">View Activity</span>
+              </a>
             </div>
             :
             <button
@@ -254,132 +254,6 @@ const Info = ({ userDetails, username }: any) => {
                         value={editProfileData.reward}
                         onChange={handleInputChange} />
                     </div>
-
-                    <div className="mt-10 text-center">
-                      <button
-                        type="button"
-                        className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                        onClick={() => setShowEditModal(false)}
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        type="submit"
-                        className="ml-2 bg-blue-500 hover:bg-blue-600 px-8 py-2 text-white rounded-md"
-                      >
-                        Update
-                      </button>
-                    </div>
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-
-      <Transition appear show={showBuyModal} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => setShowBuyModal(true)} onClick={() => setShowBuyModal(false)}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white px-6 py-8 my-5 text-left align-middle shadow-xl transition-all relative z-50">
-
-                  <form onSubmit={updateProfileInfo}>
-
-                    <div className="flex items-start mb-5">
-                      <img alt="..."
-                        src={editProfileData.updatedPhoto ? editProfileData.updatedPhoto : userDetails?.Avatar}
-                        className=" shadow-xl rounded-full h-[60px] w-[60px] align-middle border-none" />
-                      <div className="ml-2">
-                        <p>{username}</p>
-                        <p>≈{nanosToUSD(userDetails.Profile?.CoinPriceDeSoNanos, 2)} Coin Price</p>
-                      </div>
-                    </div>
-
-                    <Tab.Group>
-                      <Tab.List className="border-b px-10">
-                        <Tab as={Fragment}>
-                          {({ selected }) =>
-                            <button className={`${selected ? 'text-[#4267F7] border-b-4 border-[#4267F7]' : 'text-black'} mr-5 py-2 px-5 font-medium focus-visible:outline-none`}>
-                              Buy
-                            </button>
-                          }
-                        </Tab>
-                        <Tab as={Fragment}>
-                          {({ selected }) =>
-                            <button className={`${selected ? 'text-[#4267F7] border-b-4 border-[#4267F7]' : 'text-black'} mr-5 py-2 px-5 font-medium focus-visible:outline-none`} >
-                              Sell
-                            </button>
-                          }
-                        </Tab>
-                        <Tab as={Fragment}>
-                          {({ selected }) =>
-                            <button className={`${selected ? 'text-[#4267F7] border-b-4 border-[#4267F7]' : 'text-black'} mr-5 py-2 px-5 font-medium focus-visible:outline-none`} >
-                              Transfer
-                            </button>
-                          }
-                        </Tab>
-                      </Tab.List>
-
-                      <Tab.Panels className="mt-2 mb-16">
-                        <Tab.Panel>
-                          <PrimaryInput
-                            required
-                            hasLeftDropdown
-                            dropdownOptions={['USD', 'Deso']}
-                            id="username"
-                            name="username"
-                            label="Username"
-                            className="w-full mb-4"
-                            placeholder="Write username"
-                            value={editProfileData.userName}
-                            onChange={handleInputChange} />
-                        </Tab.Panel>
-                        <Tab.Panel>
-                          <PrimaryInput
-                            required
-                            id="username"
-                            name="username"
-                            label="Username"
-                            className="w-full mb-4"
-                            placeholder="Write username"
-                            value={editProfileData.userName}
-                            onChange={handleInputChange} />
-                        </Tab.Panel>
-                        <Tab.Panel>
-                          <PrimaryInput
-                            required
-                            id="username"
-                            name="username"
-                            label="Username"
-                            className="w-full mb-4"
-                            placeholder="Write username"
-                            value={editProfileData.userName}
-                            onChange={handleInputChange} />
-                        </Tab.Panel>
-                      </Tab.Panels>
-                    </Tab.Group>
 
                     <div className="mt-10 text-center">
                       <button
