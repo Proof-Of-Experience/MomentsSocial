c36ab772869c6bb855bb9492010e7ecd1081c2fd
Naim Lasker <naimlasker@gmail.com>
2023-09-04 18:46:49 +0600

    updated screenshot issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,15 +19,23 @@ const Home: NextPage = () => {
 
 	const [videoLoaded, setVideoLoaded] = useState<boolean>(true);
 	const [videoData, setVideoData] = useState<string[]>([]);
-	const [imageData, setImageData] = useState<string[]>([]);
+	const [momentsData, setMomentsData] = useState<string[]>([]);
+	const [loadedVideoData, setLoadedVideoData] = useState<string[]>([]);
+	const [cachedData, setCachedData] = useState<string[]>([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [currentTag, setCurrentTag] = useState<string | null>(null);
+
+	const itemsPerPage = 5;
+
 	const slider: any = useRef(null);
 
 	const momentSliderSettings = {
 		dots: false,
 		infinite: true,
+		loop: true,
 		arrows: false,
 		speed: 500,
-		slidesToShow: videoData.length > 6 ? 7 : videoData.length,
+		slidesToShow: videoData.length > 5 ? 6 : videoData.length,
 		slidesToScroll: 3,
 		responsive: [
 			{
@@ -60,7 +68,14 @@ const Home: NextPage = () => {
 		]
 	};
 
-	const fetchStatelessPostData = async () => {
+	const fetchStatelessPostData = async (page: number = 1) => {
+		if (cachedData.length >= page * itemsPerPage) {
+			// If we already have the data, don't call the API again.
+			const newData = cachedData.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+			setMomentsData(newData);
+			return;
+		}
+
 		const { getPostsStateless } = await import('deso-protocol')
 		setVideoLoaded(true)
 		const formData = {
@@ -70,15 +85,30 @@ const Home: NextPage = () => {
 		const postData = await getPostsStateless(formData)
 		setVideoLoaded(false)
 
-		if (postData?.PostsFound) {
-			const newVideoData: any = postData?.PostsFound.filter((item: any) => item.VideoURLs)
-			const newImageData: any = postData?.PostsFound.filter((item: any) => item.ImageURLs)
-			setVideoData(newVideoData)
-			setImageData(newImageData)
+		if (postData && postData.PostsFound && postData.PostsFound.length > 0) {
+			const newVideoData: any = postData?.PostsFound.filter((item: any) =>
+				item.VideoURLs && item.VideoURLs.some((videoURL: any) => videoURL)
+			);
+			setVideoData(newVideoData);
+			setCachedData(newVideoData);
+			const displayData = newVideoData.slice(0, itemsPerPage);
+			setMomentsData(displayData);
 		}
 	}
 
-	const fetchFeedData = async (tag: string) => {
+	const fetchFeedData = async (tag: string, page: number = 1) => {
+		if (currentTag !== tag) {
+			setCachedData([]); // Reset cached data
+			setCurrentTag(tag); // Update the current tag
+		}
+
+		if (cachedData.length >= page * itemsPerPage) {
+			// If we already have the data, don't call the API again.
+			const newData = cachedData.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+			setMomentsData(newData);
+			return;
+		}
+
 		setVideoLoaded(true)
 
 		const data = {
@@ -87,36 +117,65 @@ const Home: NextPage = () => {
 		const feedData = await getFeedData(data);
 		setVideoLoaded(false)
 
-		if (feedData?.HotFeedPage) {
-			const newVideoData: any = feedData?.HotFeedPage.filter((item: any) => item.VideoURLs)
-			const newImageData: any = feedData?.HotFeedPage.filter((item: any) => item.ImageURLs)
-			setVideoData(newVideoData)
-			setImageData(newImageData)
+		if (feedData && feedData.HotFeedPage && feedData.HotFeedPage.length > 0) {
+			const newVideoData: any = feedData?.HotFeedPage.filter((item: any) =>
+				item.VideoURLs && item.VideoURLs.some((videoURL: any) => videoURL)
+			);
+			setVideoData(newVideoData);
+			setCachedData(newVideoData);
+			const displayData = newVideoData.slice(0, itemsPerPage);
+			setMomentsData(displayData)
 		}
 	}
 
+	const loadMoreMoments = () => {
+		const nextPage = currentPage + 1;
+		const startIndex = (nextPage - 1) * itemsPerPage;
+		const endIndex = nextPage * itemsPerPage;
+
+		const nextDataSlice = cachedData.slice(startIndex, endIndex);
+
+		if (nextDataSlice.length === itemsPerPage) {
+			// We have all the data for the next page in cache.
+			setMomentsData(nextDataSlice);
+			setCurrentPage(nextPage);
+		} else {
+			// We don't have all the data for the next page, fetch it.
+			setCurrentPage(nextPage);
+			fetchStatelessPostData(nextPage);
+		}
+	}
+
+
 	useEffect(() => {
-		if (!router.isReady) return
+
+		if (!router.isReady) return;
 
 		if (tagParam) {
-			fetchFeedData(tagParam)
+			fetchFeedData(tagParam);
 		} else {
-			fetchStatelessPostData()
+			fetchStatelessPostData();
 		}
-	}, [router.isReady])
+	}, [router.isReady, tagParam]);
 
 	const onClickTag = (value: string) => {
+		setCurrentPage(1);
+
 		if (value === 'all') {
 			router.replace('/', undefined, { shallow: true });
 			fetchStatelessPostData()
 
 		} else {
 			router.replace({
+				pathname: router.pathname,
 				query: { ...router.query, tag: value },
 			})
 			fetchFeedData(value)
 		}
 	}
+
+	console.log('momentsData', momentsData);
+
 
 	return (
 		<MainLayout title='Moments' isLoading={videoLoaded}>
@@ -137,7 +196,7 @@ const Home: NextPage = () => {
 				}
 
 				{
-					imageData.length > 0 &&
+					momentsData.length > 0 &&
 					<>
 						<div className="flex justify-between items-center mr-10 mb-3">
 							<h3 className="font-semibold text-3xl">Moments</h3>
@@ -148,7 +207,10 @@ const Home: NextPage = () => {
 									<ChevronLeftIcon className="h-5 w-5" />
 								</button>
 								<button
-									onClick={() => slider?.current?.slickNext()}>
+									onClick={() => {
+										slider?.current?.slickNext();
+										loadMoreMoments();
+									}}>
 									<ChevronRightIcon className="h-5 w-5" />
 								</button>
 							</div>
@@ -156,7 +218,7 @@ const Home: NextPage = () => {
 
 						<Slider ref={slider}  {...momentSliderSettings}>
 							{
-								videoData.map((item: any) => {
+								momentsData.map((item: any) => {
 									return (
 										<Moment
 											key={item.PostHashHex} // use unique identifier as key
@@ -173,7 +235,7 @@ const Home: NextPage = () => {
 						</Slider>
 
 						{
-							videoData.length > 0 &&
+							momentsData.length > 0 &&
 							<hr className="border-t-2 my-10" />
 						}
 					</>
